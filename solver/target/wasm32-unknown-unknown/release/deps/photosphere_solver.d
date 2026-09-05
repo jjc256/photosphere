a1f689a806de71c75eda2ec443bfe927b082f854
Jiming Chen <jjchen256@gmail.com>
2026-09-04 23:39:28 -0400

    Exclude generated solver build output

--- solver/target/wasm32-unknown-unknown/release/deps/photosphere_solver.d
+++
@@ -1,5 +0,0 @@
-/Users/jjchen/Documents/photosphere/solver/target/wasm32-unknown-unknown/release/deps/photosphere_solver.d: src/lib.rs
-
-/Users/jjchen/Documents/photosphere/solver/target/wasm32-unknown-unknown/release/deps/photosphere_solver.wasm: src/lib.rs
-
-src/lib.rs:
--- solver/target/wasm32-unknown-unknown/release/photosphere_solver.d
+++
@@ -1,1 +0,0 @@
-/Users/jjchen/Documents/photosphere/solver/target/wasm32-unknown-unknown/release/photosphere_solver.wasm: /Users/jjchen/Documents/photosphere/solver/src/lib.rs
